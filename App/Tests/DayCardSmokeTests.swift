b4d6dc43d6b4844a4ffc6c06fba8e38f4c75a786
Dzmitry.Kharevich <dzmitry.kharevich@gmail.com>
2026-06-27 16:04:57 +0200

    Revert "fix: enable watchOS xcodebuild test for developer verify (#1)"
    This reverts commit afc6aa50d964a906b6bbdebed786e2a8fd9a725f.

--- App/Tests/DayCardSmokeTests.swift
+++
@@ -1,9 +0,0 @@
-import Shared
-import XCTest
-
-final class ActivityTrackerSmokeTests: XCTestCase {
-  func testDeepLinkOpenRouteParses() {
-    let url = URL(string: "activitytracker://open")!
-    XCTAssertEqual(DeepLink(url: url), .open)
-  }
-}
